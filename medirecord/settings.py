460b9811d4ce6c6525577db3690f16c7a0a714f1
Kevin Flores <becino@gmail.com>
2015-02-23 22:15:23 -0600

    Configuracion de la base de datos para heroku

--- medirecord/settings.py
+++ medirecord/settings.py
@@ -71,9 +71,16 @@ WSGI_APPLICATION = 'medirecord.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd689bqgqcrpuu9',
+        'USER': 'dfraoofrzztoil',
+        'PASSWORD': 'uYgKe7OcHgYBIoW5b0EES5nqlC',
+        'HOST': 'ec2-107-22-249-214.compute-1.amazonaws.com'),
     }
 }
 
